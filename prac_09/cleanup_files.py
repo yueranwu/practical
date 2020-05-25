73b7af9f7c9471921d7c18d51406b25a4f67ed42
Yueran <yueran.wu@my.jcu.edu.au>
2020-05-25 19:22:01 +0800

    Add rename file program

--- prac_09/cleanup_files.py
+++ prac_09/cleanup_files.py
@@ -2,7 +2,6 @@
 CP1404/CP5632 Practical
 Reformat file names
 """
-import shutil
 import os
 
 
@@ -10,23 +9,33 @@ def main():
     """Process all subdirectories using os.walk()."""
     os.chdir('Lyrics')
     for directory_name, subdirectories, filenames in os.walk('.'):
-        # print("Directory:", directory_name)
-        # print("\tcontains subdirectories:", subdirectories)
-        # print("\tand files:", filenames)
-        # print("(Current working directory is: {})".format(os.getcwd()))
-
         for file in filenames:
-            new_name = get_fixed_filename(file)
-            print("New name is: {}".format(new_name))
-        #   path = os.path.join(directory_name, file)
-        #   new_name = os.path.join(directory_name, get_fixed_filename(file))
-        #   os.rename(path, new_name)
+            # join the path
+            path = os.path.join(directory_name, file)
+            # get new name
+            new_name = os.path.join(directory_name, get_fixed_filename(file))
+            # replace new name with old name
+            os.rename(path, new_name)
 
 
 def get_fixed_filename(filename):
     """Return a 'fixed' version of filename."""
-    new_name = filename.replace(" ", "_").replace(".TXT", ".txt")
-    print(new_name)
+    name = filename.replace(" ", "_").replace(".TXT", ".txt")
+    new_name = ""
+    # iterate through each character in the name
+    for index, char in enumerate(name):
+        new_name += char
+        try:
+            # reformat through adding underscore
+            if name[index].islower() and name[index + 1].isupper():
+                new_name += "_"
+            # reformat through changing the first letter after underscore to uppercase
+            elif name[index] == "_" and name[index + 1].islower():
+                name[index + 1].upper()
+
+        # handle index error
+        except IndexError:
+            pass
 
     return new_name
 
